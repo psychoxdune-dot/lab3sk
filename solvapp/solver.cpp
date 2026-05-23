ce6e2936136bbdde3434d22afb3f73b9f889ee37
psychoxdune-dot <psychoxdune@gmail.com>
2026-05-24 02:16:49 +0300

    Delete solvapp directory

--- solvapp/solver.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "formatter_ex.hpp"
-#include "solver.hpp"
-int main() {
-    int result = solve(2, 3);
-    std::cout << format_ex("Result: " + std::to_string(result)) << std::endl;
-    return 0;
-}
